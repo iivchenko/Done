6d049aa9af487124c279950ee4dc84d000b6ed19
Ivan Ivchenko <ivan.ivchenko@betssongroup.com>
2018-04-11 09:39:39 +0300

    Move project

---
+++ Src/MyTaskList/Models/TasksContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace MyTaskList.Models
+{
+    public sealed class TasksContext : DbContext
+    {
+        public TasksContext()
+            : base("Default")
+        {
+        }
+
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
---
+++ Src/MyTaskList/Models/TasksInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace MyTaskList.Models
+{
+    // TODO: Remove the initializer. Provide new db initializer with starter kit: welcom task, basic tags (status.inprogress status.done, priority.low, priority.middle) etc.
+    public sealed class TasksInitializer : DropCreateDatabaseAlways<TasksContext>
+    {
+        protected override void Seed(TasksContext db)
+        {
+            db.Tasks.Add(new TaskItem {Name = "Task 1", Status = TaskItemStatus.Done});
+            db.Tasks.Add(new TaskItem {Name = "Task 2", Status = TaskItemStatus.InProgress});
+
+            base.Seed(db);
+        }
+    }
+}
