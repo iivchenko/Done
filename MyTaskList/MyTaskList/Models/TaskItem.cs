89e973be20d746af549930098990062c4274e7ba
Ivan Ivchenko <ivan.ivchenko@betssongroup.com>
2018-04-11 08:50:32 +0300

    Initial commit of old My Task List from Learning-ASP.NET

---
+++ MyTaskList/MyTaskList/Models/TaskItem.cs
@@ -0,0 +1,13 @@
+﻿namespace MyTaskList.Models
+{
+    public sealed class TaskItem
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public TaskItemStatus Status { get; set; }
+    }
+}
---
+++ MyTaskList/MyTaskList/Models/Tasks/IndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MyTaskList.Models.Pagination;
+using System.Collections.Generic;
+
+namespace MyTaskList.Models.Tasks
+{
+    public sealed class IndexViewModel
+    {
+        public int Total { get; set; }
+
+        public IEnumerable<TaskItem> Tasks { get; set; }
+
+        public PageViewModel Pagination { get; set; }
+
+        public string Pattern { get; set; }
+    }
+}
