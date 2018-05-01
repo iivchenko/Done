81c67c066dd79daf3e99ddc424301e666fcde1ef
iivchenko <iivchenko@live.com>
2018-05-01 20:39:54 +0300

    Move Data project to EF Core

--- Src/Done.Data/GoalsContext.cs
+++ Src/Done.Data/GoalsContext.cs
@@ -1,12 +1,13 @@
-﻿using System.Data.Entity;
-using Done.Domain;
+﻿using Done.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace Done.Data
 {
+    // TODO: Provide new db initializer with starter kit: welcom task, basic tags (status.inprogress status.done, priority.low, priority.middle) etc.
     public sealed class DoneContext : DbContext
     {
-        public DoneContext(string name)
-            : base(name)
+        public DoneContext(DbContextOptions<DoneContext> options)
+            : base(options)
         {            
         }
 
